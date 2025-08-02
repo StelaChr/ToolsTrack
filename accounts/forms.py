748c98a6aed47b61ef42dafb89765ee946bffcae
Stela <stella_chr@abv.bg>
2025-08-03 00:12:23 +0300

    Changes in accounts/forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms import PasswordInput
 
 UserModel = get_user_model()
 
@@ -7,6 +8,13 @@ class AppUserCreationForm(UserCreationForm):
     class Meta:
         model = UserModel
         fields = [ "email"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+        self.fields['password1'].help_text = "* Min 8 characters, at least one non-digit ."
+
 
 
 
