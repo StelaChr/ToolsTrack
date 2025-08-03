caa9486583a36f14d3e4d6afbd2a2a00c27932f4
Stela <stella_chr@abv.bg>
2025-08-03 16:58:04 +0300

    Additional changes in .env and settings.py file, also README file for the Azure deployment

--- ToolsTrack/settings.py
+++ ToolsTrack/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
-CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS")
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS").split(",")
 
 # Application definition
 
