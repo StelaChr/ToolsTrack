f74eca7b07b4479c6a3e9ae8a88c36cc368372ef
Stela <stella_chr@abv.bg>
2025-08-05 22:47:10 +0300

    changed in maintanenance admin

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -7,3 +7,5 @@ class MaintenanceAdmin(admin.ModelAdmin):
     list_display = ("tool", "maintenance_type", "performed_by","cost")
     list_filter = ("maintenance_type", "performed_by","cost")
     search_fields = ("tool", "maintenance_type", "performed_by","cost")
+    ordering = ("cost", )
+    list_editable = ("cost",)
